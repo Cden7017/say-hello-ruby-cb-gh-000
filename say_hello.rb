013c00d0fc9b475ca5c5a82a71a9d24418f79d2a
Vishal Wavhale <cden7017@gmail.com>
2018-12-27 17:50:15 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,9 @@
 # Build your say_hello method here
 def say_hello(name)
-  prints("Hello "+name+"!");
+  if(name!= "")
+    print("Hello "+name+"!");
+  else
+    print("Hello Ruby Programmer");
 end
 
 say_hello("Vishal")
